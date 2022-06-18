be3e5bdad10555164771a3959df66a4671b737fb
Mehmet Seven <mehmetseven0@gmail.com>
2022-06-18 15:24:38 +0300

    formik forms

--- group1/day3/forms/src/components/FormWithFormik.js
+++ group1/day3/forms/src/components/FormWithFormik.js
@@ -1,52 +1,140 @@
 import React from "react";
-import { Formik } from "formik";
-import validations from "./validations";
+import { useFormik } from "formik";
+import validationSchema from "./validations";
 
 function FormWithFormik() {
+  const {
+    handleSubmit,
+    handleChange,
+    handleBlur,
+    setFieldValue,
+    values,
+    errors,
+    touched,
+    isSubmitting,
+  } = useFormik({
+    initialValues: {
+      name: "test",
+      email: "testtt@test.com",
+      message: "deneme",
+      campaigns: "0",
+      monthlyOrWeekly: "",
+    },
+    onSubmit: async (values, bag) => {
+      // await new Promise((r) => setTimeout(r, 1000));
+      console.log("values", values);
+
+      if (values.email === "test@test.com") {
+        return bag.setFieldError("email", "Bu mail adresi zaten kullanımda.");
+      }
+
+      // bag.resetForm();
+    },
+    validationSchema,
+  });
+
   return (
-    <Formik
-      initialValues={{
-        name: "",
-        email: "",
-        message: "",
-      }}
-      onSubmit={(values) => {
-        console.log("values", values);
-      }}
-      validationSchema={validations}
-    >
-      {({ values, handleChange, handleSubmit }) => (
-        <form className="contact-form" onSubmit={handleSubmit}>
-          <div>
-            <label htmlFor="name">Name</label>
-            <input
-              id="name"
-              value={values.name}
-              onChange={handleChange("name")}
-            />
-          </div>
-          <div>
-            <label htmlFor="email">E-Mail</label>
-            <input
-              id="email"
-              value={values.email}
-              onChange={handleChange("email")}
-            />
-          </div>
-          <div>
-            <label htmlFor="message">Message</label>
-            <textarea
-              id="message"
-              value={values.message}
-              onChange={handleChange("message")}
-            />
-          </div>
-          <div>
-            <button type="submit">Save</button>
-          </div>
-        </form>
+    <form className="contact-form" onSubmit={handleSubmit}>
+      <pre>{JSON.stringify(values, null, 2)}</pre>
+
+      <div>
+        <label htmlFor="name">Name</label>
+        <input
+          id="name"
+          type="text"
+          disabled={isSubmitting}
+          value={values.name}
+          onChange={handleChange("name")}
+          onBlur={handleBlur("name")}
+          placeholder="İsim"
+        />
+        {errors.name && touched.name && (
+          <div className="error">{errors.name}</div>
+        )}
+      </div>
+      <div>
+        <label htmlFor="email">E-Mail</label>
+        <input
+          id="email"
+          type="text"
+          disabled={isSubmitting}
+          value={values.email}
+          onChange={handleChange("email")}
+          onBlur={handleBlur("email")}
+          placeholder="E-mail"
+        />
+        {errors.email && touched.email && (
+          <div className="error">{errors.email}</div>
+        )}
+      </div>
+      <div>
+        <label htmlFor="message">Message</label>
+        <textarea
+          id="message"
+          disabled={isSubmitting}
+          value={values.message}
+          onChange={handleChange("message")}
+          onBlur={handleBlur("message")}
+          placeholder="Message"
+        />
+        {errors.message && touched.message && (
+          <div className="error">{errors.message}</div>
+        )}
+      </div>
+
+      <div>
+        <label htmlFor="campaigns">Kampanyalar</label>
+        <input
+          type="radio"
+          name="campaigns"
+          value="0"
+          defaultChecked={values.campaigns === "0"}
+          onChange={() => {
+            setFieldValue("monthlyOrWeekly", "");
+            setFieldValue("campaigns", "0");
+          }}
+        />{" "}
+        Hayır
+        <input
+          type="radio"
+          name="campaigns"
+          value="1"
+          defaultChecked={values.campaigns === "1"}
+          onChange={handleChange("campaigns")}
+        />{" "}
+        Evet
+        {errors.campaigns && <div className="error">{errors.campaigns}</div>}
+      </div>
+
+      {values.campaigns === "1" && (
+        <div>
+          <label htmlFor="">Aylık/Haftalık</label>
+          <input
+            type="radio"
+            value="monthly"
+            name="monthlyOrWeekly"
+            onChange={handleChange("monthlyOrWeekly")}
+          />{" "}
+          Aylık
+          <input
+            type="radio"
+            value="weekly"
+            name="monthlyOrWeekly"
+            onChange={handleChange("monthlyOrWeekly")}
+          />{" "}
+          Haftalık
+          {errors.monthlyOrWeekly && (
+            <div className="error">{errors.monthlyOrWeekly}</div>
+          )}
+        </div>
       )}
-    </Formik>
+
+      <div>
+        <button type="submit" disabled={isSubmitting}>
+          Save
+        </button>
+      </div>
+    </form>
   );
 }
 
--- group1/day3/forms/src/components/validations.js
+++ group1/day3/forms/src/components/validations.js
@@ -1,9 +1,23 @@
-import { object, string } from "yup";
+import * as yup from "yup";
 
-const validations = object({
-  name: string().required(),
-  email: string().email().required(),
-  message: string().min(5).required(),
+const messages = {
+  required: "Bu alan zorunludur.",
+  email: "Geçerli bir email belirtin.",
+  min: "En az 5 karakter giriniz.",
+};
+
+const validations = yup.object({
+  name: yup.string().required(messages.required),
+  email: yup.string().email(messages.email).required(messages.required),
+  message: yup.string().min(5, messages.min).required(messages.required),
+  campaigns: yup.string().required(),
+  monthlyOrWeekly: yup
+    .string()
+    .nullable()
+    .when("campaigns", {
+      is: "1",
+      then: yup.string().required("Zorunlu"),
+    }),
 });
 
 export default validations;

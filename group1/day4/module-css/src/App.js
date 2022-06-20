e37901600e06dba40523feb0d8448ffc95f972e7
Mehmet Seven <mehmetseven0@gmail.com>
2022-06-20 18:59:41 +0300

    module css

---
+++ group1/day4/module-css/src/App.js
@@ -0,0 +1,13 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
